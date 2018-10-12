a676dfb54ef0a7556b315cf24d3352ffb13bb6dc
jmacey <jmacey@bournemouth.ac.uk>
2018-10-12 10:34:36 +0100

    Working with C++ 17 and smart pointers

--- Image.pro
+++ Image.pro
@@ -3,3 +3,4 @@ SOURCES += main.cpp
 OTHER_FILES+=README.md
 QMAKE_CXXFLAGS += $$system(pkg-config --cflags Magick++)
 LIBS+= $$system(pkg-config --libs Magick++)
+CONFIG+=c++14
--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <cstdlib>
 #include <Magick++.h>
+#include <memory>
 
 int main()
 {
     const size_t width=400;
     const size_t height=400;
     const size_t pixelDepth=3;
-    unsigned char image[width*height*pixelDepth];    
+    std::unique_ptr<unsigned char []> image=
+            std::make_unique<unsigned char []>(width*height*pixelDepth);
 
     for(size_t i=0; i<width*height*pixelDepth; ++i)
     {
-        image[i]=128;
+        image[i]=8;
     }
 
-    Magick::Image output(width,height,"RGB",Magick::CharPixel,image);
+    Magick::Image output(width,height,"RGB",Magick::CharPixel,image.get());
     output.depth(16);
     output.write("test.bmp");
     return EXIT_SUCCESS;
